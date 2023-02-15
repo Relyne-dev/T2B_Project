e1acef6a47582ca85037acf31e118e08103088ee
G3X787 <davidekrent@gmail.com>
2023-02-14 19:16:50 -0500

    removed extra comments

--- T2B_Project/Assets/Scripts/GameManager.cs
+++ T2B_Project/Assets/Scripts/GameManager.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static List<GameObject> cards; 
+    public static List<GameObject> cards;
     public static GameObject player;
 
 
 
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
